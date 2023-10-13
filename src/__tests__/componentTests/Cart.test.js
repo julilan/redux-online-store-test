b071365a90600f552e2275db2659595dc7dbfbe1
julilan <juli.molnar@outlook.com>
2023-10-13 13:11:17 +0300

    add tests for slices and Cart

---
+++ src/__tests__/componentTests/Cart.test.js
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import Cart from '../../components/Cart';
+import { cartItems } from '../../testData/cartData';
+
+const mockStore = configureStore([]);
+
+describe('Cart initialisation', () => {
+  it('test should pass', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Cart should be empty when the cart is empty', () => {
+    const store = mockStore({
+      cart: {
+        cart: [],
+      }
+    });
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Cart />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    expect(screen.getByText('Your cart is empty')).toBeInTheDocument();
+  });
+
+  it('Should render a list of products from the cart', () => {
+    const store = mockStore({
+      cart: {
+        cart: cartItems
+      }
+    })
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Cart />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    expect(screen.getAllByTestId('product')).toHaveLength(2);
+  });
+});
---
+++ src/__tests__/sliceTests/productSlice.test.js
@@ -0,0 +1,66 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { fetchProducts } from '../../features/productsSlice';
+
+// We need to use thunk middleware to test async actions
+const mockStore = configureStore([thunk]);
+
+describe('a test', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe('fetchProducts', () => {
+  beforeEach(() => {
+    jest.setTimeout(10000); // Increase timeout for slow API calls
+  });
+
+  it('should fetch products from the API', async () => {
+    const store = mockStore({ products: [] });
+    await store.dispatch(fetchProducts());
+
+    const actions = store.getActions();
+    expect(actions[0].type).toEqual('products/fetchProducts/pending');
+    expect(actions[1].type).toEqual('products/fetchProducts/fulfilled');
+
+    const products = actions[1].payload;
+    expect(products).toHaveLength(20);
+  });
+
+  it('Products should contain data that is equal to our test case', async () => {
+    const store = mockStore({ products: [] });
+    await store.dispatch(fetchProducts());
+
+    const actions = store.getActions();
+    const products = actions[1].payload;
+    products.forEach((product) => {
+      expect(product).toHaveProperty('id');
+      expect(product).toHaveProperty('title');
+      expect(product).toHaveProperty('price');
+      expect(product).toHaveProperty('description');
+      expect(product).toHaveProperty('category');
+      expect(product).toHaveProperty('image');
+    });
+  });
+  it('Product 1 has specifi data to our test case', async () => {
+    const store = mockStore({ products: [] });
+    await store.dispatch(fetchProducts());
+
+    const actions = store.getActions();
+    const products = actions[1].payload;
+
+    expect(products[0].id).toEqual(1);
+    expect(products[0].title).toEqual(
+      'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops'
+    );
+    expect(products[0].price).toEqual(109.95);
+    expect(products[0].description).toEqual(
+      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday'
+    );
+    expect(products[0].category).toEqual("men's clothing");
+    expect(products[0].image).toEqual(
+      'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'
+    );
+  });
+});
